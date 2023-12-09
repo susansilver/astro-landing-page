757ed9224bab302b647f611d87f132e6efa035ff
Susan Silver <hello@susansilver.net>
2023-12-09 13:47:34 -0800

    update meta and og data

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://mathcommunications.com/",
   integrations: [tailwind()],
   vite: {
     ssr: {
